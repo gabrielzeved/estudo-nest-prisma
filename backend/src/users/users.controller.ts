066ed498cb45fd5badc86b347c3a4dfd009e92f4
Gabriel <gabrielsilveira@id.uff.br>
2022-01-06 03:28:47 -0300

    adiciona service do prisma e inicia service e controller de user

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { UserService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+	constructor(private readonly userService: UserService) {}
+
+	@Get(':id')
+	getUserById(@Param('id') id: string): Promise<User> {
+		return this.userService.one({
+			id: Number(id),
+		});
+	}
+
+	@Post()
+	createUser(@Body() userData: Prisma.UserCreateInput): Promise<User> {
+		return this.userService.new(userData);
+	}
+}
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,27 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
+import { User, Prisma } from '@prisma/client';
 
 @Injectable()
-export class UsersService {
-	users: User[] = [];
+export class UserService {
+	constructor(private prisma: PrismaService) {}
 
-	all(): User[] {
-		return this.users;
+	async one(
+		userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+	): Promise<User | null> {
+		return this.prisma.user.findUnique({
+			where: userWhereUniqueInput,
+		});
 	}
 
-	one(id: string): User {
-		return this.users.find((user) => user.id === id);
+	async users(params: {
+		skip?: number;
+		take?: number;
+		cursor?: Prisma.UserWhereUniqueInput;
+		where?: Prisma.UserWhereInput;
+		orderBy?: Prisma.UserOrderByWithRelationInput;
+	}): Promise<User[]> {
+		const { skip, take, cursor, where, orderBy } = params;
+		return this.prisma.user.findMany({
+			skip,
+			take,
+			cursor,
+			where,
+			orderBy,
+		});
 	}
 
-	new(user: User) {
-		if (!this.exists(user.id)) {
-			this.users.push(user);
-			return user;
-		}
-
-		return null;
+	async new(data: Prisma.UserCreateInput): Promise<User> {
+		return this.prisma.user.create({
+			data,
+		});
 	}
 
-	exists(id: string) {
-		return this.users.some((user) => user.id === id);
+	async update(params: {
+		where: Prisma.UserWhereUniqueInput;
+		data: Prisma.UserUpdateInput;
+	}): Promise<User> {
+		const { where, data } = params;
+		return this.prisma.user.update({
+			where,
+			data,
+		});
+	}
+
+	async delete(where: Prisma.UserWhereUniqueInput): Promise<User> {
+		return this.prisma.user.delete({
+			where,
+		});
 	}
 }
