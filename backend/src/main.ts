0e9e12ffea8c26371cdc601d9869da91b2162371
Gabriel <gabrielsilveira@id.uff.br>
2022-01-15 06:15:56 -0300

    adiciona swagger e validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	const config = new DocumentBuilder()
+		.setTitle('Example')
+		.setDescription('The API description')
+		.setVersion('1.0')
+		.addTag('Example')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, document);
+
+	app.useGlobalPipes(new ValidationPipe());
+	await app.listen(3000);
 }
 bootstrap();
